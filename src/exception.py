29921b7fc7e03c9c02e035da244e524f9c5db525
patharepremkumar <patharepremkumar@gmail.com1>
2023-03-26 09:41:07 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tb = error_details.exc_info()
@@ -16,12 +17,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-
-
-
-
-
-    
-
-    
